27fa44575b9a66a166e5a8242a445bac8d8f4585
suropark <xoopp5818@gmail.com>
2022-05-20 22:18:39 +0900

    update interfaces

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.0;
 interface IvMesh {
     function lockMESH(uint256 _amount, uint256 _period) external; // 1555200000 is Max Period (8x)
     function claimReward() external;
+    function getCurrentBalance(address _usr) external view returns(uint);
+    function totalSupply() external view returns(uint);
+    function lockedKSP(address _usr) external view returns (uint);
+    function userRewardSum(address _usr) external view returns (uint);
+    
 
 }
 
@@ -13,5 +18,7 @@ interface IPoolVoting {
     function removeVoting(address _exchange, uint _amount) external;
     function claimReward(address _exchange) external;
     function claimRewardAll() external;
+    function removeAllVoting()external; 
+
 }
 
